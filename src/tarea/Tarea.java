bcc9869f1d75b7879e3b7ef042a0a8d8f361f576
JonhAlva <jonathanchacon638@gmail.com>
2022-02-24 19:50:02 -0600

    Tarea2

---
+++ src/tarea/Tarea.java
@@ -0,0 +1,11 @@
+package tarea;
+
+public class Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Aqui inicia el programa");
+        Login Login = new Login();
+        Login.setVisible(true);
+    }
+    
+}
